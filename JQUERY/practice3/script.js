10a519d8dcc3a4753f62ca0988aac3b6a3cc48ad
BIT Student <classroom@bgit.rs>
2018-05-23 18:07:12 +0200

    web

---
+++ JQUERY/practice3/script.js
@@ -0,0 +1,29 @@
+ $(function(){
+
+//     var $box = $(".box");
+//     $box.on("mouseover", function(){
+//         $box.animate({
+//             top: "250px",
+//             left: "300px",
+//             width: "400px",
+//             height: "300px"
+//         }, 500)
+
+//     })
+
+//     $box.on("mouseleave", function(){
+//         $box.animate({
+//             top:0,
+//             left:0,
+//             width:"200px",
+//             height: "200px"
+//         }, 500)
+//     })
+
+// })
+
+$('.slider').bxSlider({
+    adaptiveHeight: true,
+});
+    // Options will go here
+});
